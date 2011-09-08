4ab055588ba6a1d023d59963edd5e7c2f047aa18
unknown <sdp@.(none)>
2011-09-08 22:26:47 +0200

    Added validation Added "byte" filling

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -34,12 +34,14 @@
 			this.label2 = new System.Windows.Forms.Label();
 			this.txtHex = new System.Windows.Forms.TextBox();
 			this.label3 = new System.Windows.Forms.Label();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Location = new System.Drawing.Point(3, 0);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(39, 13);
 			this.label1.TabIndex = 0;
@@ -47,30 +49,30 @@
 			// 
 			// txtBinary
 			// 
-			this.txtBinary.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtBinary.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtBinary.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtBinary.Location = new System.Drawing.Point(13, 30);
+			this.txtBinary.Location = new System.Drawing.Point(3, 16);
 			this.txtBinary.Name = "txtBinary";
-			this.txtBinary.Size = new System.Drawing.Size(473, 33);
+			this.txtBinary.Size = new System.Drawing.Size(448, 33);
 			this.txtBinary.TabIndex = 1;
 			this.txtBinary.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBinary_KeyUp);
 			// 
 			// txtDecimal
 			// 
-			this.txtDecimal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtDecimal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtDecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtDecimal.Location = new System.Drawing.Point(13, 85);
+			this.txtDecimal.Location = new System.Drawing.Point(3, 68);
 			this.txtDecimal.Name = "txtDecimal";
-			this.txtDecimal.Size = new System.Drawing.Size(473, 33);
+			this.txtDecimal.Size = new System.Drawing.Size(448, 33);
 			this.txtDecimal.TabIndex = 3;
 			this.txtDecimal.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtDecimal_KeyUp);
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(10, 70);
+			this.label2.Location = new System.Drawing.Point(3, 52);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(48, 13);
 			this.label2.TabIndex = 2;
@@ -78,42 +80,61 @@
 			// 
 			// txtHex
 			// 
-			this.txtHex.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
+			this.txtHex.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtHex.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtHex.Location = new System.Drawing.Point(12, 141);
+			this.txtHex.Location = new System.Drawing.Point(3, 120);
 			this.txtHex.Name = "txtHex";
-			this.txtHex.Size = new System.Drawing.Size(473, 33);
+			this.txtHex.Size = new System.Drawing.Size(448, 33);
 			this.txtHex.TabIndex = 5;
 			this.txtHex.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtHex_KeyUp);
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(13, 125);
+			this.label3.Location = new System.Drawing.Point(3, 104);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(29, 13);
 			this.label3.TabIndex = 4;
 			this.label3.Text = "Hex:";
 			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.txtHex, 0, 5);
+			this.tableLayoutPanel1.Controls.Add(this.txtBinary, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label3, 0, 4);
+			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.txtDecimal, 0, 3);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 6;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(454, 158);
+			this.tableLayoutPanel1.TabIndex = 6;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(498, 184);
-			this.Controls.Add(this.txtHex);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.txtDecimal);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.txtBinary);
-			this.Controls.Add(this.label1);
+			this.ClientSize = new System.Drawing.Size(454, 158);
+			this.Controls.Add(this.tableLayoutPanel1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "Form1";
 			this.Text = "SNSC: Simple Numeral System Converter";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -125,6 +146,7 @@
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.TextBox txtHex;
 		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 	}
 }
 
--- src/Form1.cs
+++ src/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace SNSC
@@ -9,47 +10,112 @@ namespace SNSC
 		{
 			InitializeComponent();
 		}
-		
-		private void updateValues(long value, string origin)
+
+		private int CalculateNumberOfBytes(long value)
 		{
-			if(origin != "binary")
-				txtBinary.Text = Convert.ToString(value, 2);
-
-			if(origin != "decimal")
-				txtDecimal.Text = value.ToString();
-
-			if(origin != "hex")
-				txtHex.Text = Convert.ToString(value, 16).ToUpper();
+			return (int)Math.Floor(Math.Log(value, 2) / 8) + 1;
 		}
 
-		private void txtHex_KeyUp(object sender, KeyEventArgs e)
+		private void ConvertValue(long inputValue, NumericBase numericBase)
 		{
-			try
+			var numberOfBytesUsed = CalculateNumberOfBytes(inputValue);
+
+			switch (numericBase)
 			{
-				updateValues(Convert.ToInt64(txtHex.Text, 16), "hex");
+				case NumericBase.Binary:
+					UpdateDecimalBox(inputValue);
+					UpdateHexBox(inputValue, numberOfBytesUsed);
+					break;
+				case NumericBase.Decimal:
+					UpdateBinaryBox(inputValue, numberOfBytesUsed);
+					UpdateHexBox(inputValue, numberOfBytesUsed);
+					break;
+				case NumericBase.Hex:
+					UpdateBinaryBox(inputValue, numberOfBytesUsed);
+					UpdateDecimalBox(inputValue);
+					break;
 			}
-			catch
-			{ }
 		}
 
-		private void txtDecimal_KeyUp(object sender, KeyEventArgs e)
+		private void UpdateBinaryBox(long outputValue, int numberOfBytes)
 		{
-			try
-			{
-				updateValues(Convert.ToInt64(txtDecimal.Text), "decimal");
-			}
-			catch
-			{ }
+			var binString = Convert.ToString(outputValue, 2);
+
+			txtBinary.Text = binString.PadLeft(8 * numberOfBytes, '0');
+		}
+
+		private void UpdateDecimalBox(long outputValue)
+		{
+			txtDecimal.Text = outputValue.ToString();
+		}
+
+		private void UpdateHexBox(long outputValue, int numberOfBytes)
+		{
+			txtHex.Text = outputValue.ToString("X" + (2 * numberOfBytes));
 		}
 
 		private void txtBinary_KeyUp(object sender, KeyEventArgs e)
 		{
-			try
+			if (Regex.IsMatch(txtBinary.Text, @"^[01]*$"))
 			{
-				updateValues(Convert.ToInt64(txtBinary.Text, 2), "binary");
+				if (txtBinary.Text.Length > 0)
+				{
+					try
+					{
+						ConvertValue(Convert.ToInt64(txtBinary.Text, 2), NumericBase.Binary);
+					}
+					catch (OverflowException)
+					{ }
+				}
 			}
-			catch
-			{ }
 		}
+
+		private void txtDecimal_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (Regex.IsMatch(txtDecimal.Text, @"^\d*$"))
+			{
+				if (txtDecimal.Text.Length > 0)
+				{
+					try
+					{
+						var inputValue = Convert.ToInt64(txtDecimal.Text);
+
+						ConvertValue(inputValue, NumericBase.Decimal);
+					}
+					catch (OverflowException)
+					{ }
+				}
+			}
+		}
+
+		private void txtHex_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (Regex.IsMatch(txtHex.Text, @"^[\dA-Fa-f]*$"))
+			{
+				if (txtHex.Text.Length > 0)
+				{
+					try
+					{
+						var inputValue = Convert.ToInt64(txtHex.Text, 16);
+
+						ConvertValue(inputValue, NumericBase.Hex);
+					}
+					catch (OverflowException)
+					{ }
+				}
+			}
+		}
+
+		#region Nested type: NumericBase
+
+		private enum NumericBase
+		{
+			Binary,
+			Octet,
+			Decimal,
+			Hex
+		}
+
+		#endregion
 	}
 }
